061e1b7aea05225522014fee8fb20c966edc51db
saxenasom <129320068+saxenasom@users.noreply.github.com>
2023-03-30 01:19:12 +0530

    Add files via upload

---
+++ nifty.sql
@@ -0,0 +1,26 @@
+CREATE TABLE nse(
+                record_date date,
+                day_open decimal(8,2),
+                high decimal(8,2),
+                low decimal(8,2),
+                day_close decimal(8,2)
+);
+
+COPY nse 
+from 'D:\Greenblatt\Raw\NIFTY 50_Data.csv'
+with (format CSV,header);
+
+
+SELECT 'NIFTY' as symbol,make_date(yr,mnth,1) as proxy,price,NULL
+from
+(
+	SELECT 
+		CAST(date_part('year',record_date) AS integer) AS yr,
+		CAST(date_part('month',record_date) AS integer) AS mnth, 
+		CAST(percentile_cont(0.5) WITHIN GROUP (ORDER BY day_close) AS numeric(8,2)) AS price
+	FROM nse
+	GROUP BY yr, mnth
+)t1
+
+
+
